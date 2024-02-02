64745d6991bb570302230195f6f4b37fd5dedea8
svetlichnaya.org@gmail.com <svetlichnaya.org@gmail.com>
2024-02-02 15:31:04 +0100

    add code

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO Categories
+Values('House'),
+('Apartment');
+
+Select * from Categories;
+Select * from Houses;
+Select * from AspNetUsers;
+Select * from Addresses;
+Select * from Rents;
+Select * from Tags;
+
+INSERT INTO Addresses (Latitude, Longitude, Country, City, FormattedAddress, AddressLabel) VALUES
+('40.712776', '-74.005974', 'USA', 'New York', 'New York, NY, USA', 'NYC Label'),
+('48.856614', '2.352222', 'France', 'Paris', 'Paris, France', 'Paris Label'),
+('51.507351', '-0.127758', 'UK', 'London', 'London, UK', 'London Label'),
+('35.689487', '139.691711', 'Japan', 'Tokyo', 'Tokyo, Japan', 'Tokyo Label');
