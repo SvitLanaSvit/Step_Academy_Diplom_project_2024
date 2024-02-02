197b8aad16d8ec2874d9b95a440aa30c724335c1
svetlichnaya.org@gmail.com <svetlichnaya.org@gmail.com>
2024-02-02 16:26:05 +0100

    changes

--- Diplom_project_2024/Data/User.cs
+++ Diplom_project_2024/Data/User.cs
@@ -4,6 +4,7 @@ namespace Diplom_project_2024.Data
 {
     public class User : IdentityUser
     {
+        //public override string Id { get => base.Id; set => base.Id = value; }
         public string DisplayName { get; set; } = default!;
         public List<House>? Houses { get; set; }
         public List<Rent>? Rents { get; set; }
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -14,3 +14,7 @@ INSERT INTO Addresses (Latitude, Longitude, Country, City, FormattedAddress, Add
 ('48.856614', '2.352222', 'France', 'Paris', 'Paris, France', 'Paris Label'),
 ('51.507351', '-0.127758', 'UK', 'London', 'London, UK', 'London Label'),
 ('35.689487', '139.691711', 'Japan', 'Tokyo', 'Tokyo, Japan', 'Tokyo Label');
+
+INSERT INTO AspNetUsers(Id, Email, EmailConfirmed, DisplayName, UserName, AccessFailedCount, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled)
+Values('1','dmytro.vychkin@gmail.com', 1, 'dmytro', 'dmytro', 1, 1, 0, 0),
+('2','svitlana.kizilpinar@gmail.com', 1, 'sviti', 'svitlana', 1, 1, 0, 0);
