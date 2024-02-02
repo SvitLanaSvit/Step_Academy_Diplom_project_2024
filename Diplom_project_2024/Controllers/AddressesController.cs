8021ddb38ad0642127842ad6f760a6df778c73be
svetlichnaya.org@gmail.com <svetlichnaya.org@gmail.com>
2024-02-02 15:15:04 +0100

    adding controllers

---
+++ Diplom_project_2024/Controllers/AddressesController.cs
@@ -0,0 +1,104 @@
+﻿using Diplom_project_2024.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Diplom_project_2024.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly HousesDBContext _context;
+
+        public AddressesController(HousesDBContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/Addresses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Address>>> GetAddresses()
+        {
+            return await _context.Addresses.ToListAsync();
+        }
+
+        //GET: api/Addresses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Address>> GetAddress(int id)
+        {
+            var address = await _context.Addresses.FindAsync(id);
+
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return address;
+        }
+
+        //POST: api/Addresses
+        [HttpPost]
+        public async Task<ActionResult<Address>> PostAddress(Address address)
+        {
+            _context.Addresses.Add(address);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAddress), new { id = address.Id }, address);
+        }
+
+        //PUT: api/Addresses
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Address>> PutAddress(int id, Address address)
+        {
+            if (id != address.Id)
+            {
+                return BadRequest("Address ID mismatch");
+            }
+
+            var addressFromDB = await _context.Addresses.FindAsync(id);
+            if (addressFromDB == null)
+            {
+                return NotFound();
+            }
+
+            _context.Entry(addressFromDB).CurrentValues.SetValues(address);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Addresses.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            await _context.Entry(addressFromDB).ReloadAsync();
+
+            // Return the updated address
+            return addressFromDB;
+        }
+
+        //DELETE: api/Addresses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAddress(int id)
+        {
+            var address = await _context.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            _context.Addresses.Remove(address);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
