92424795de626791b93e0d9df77b70ea366ea455
The Waggle <artuoh76@gmail.com>
2024-02-07 02:16:55 +0100

    Identity

---
+++ Diplom_project_2024/Models/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Diplom_project_2024.Models.DTOs
+{
+    public class UserRegisterDTO
+    {
+        public string UserName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string DisplayName { get; set; } = default!;
+
+    }
+}
