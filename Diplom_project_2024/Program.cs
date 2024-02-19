f77f7be7fc11ddce7b22cbfc9fa9115c01ac9969
The Waggle <artuoh76@gmail.com>
2024-02-19 21:45:35 +0100

    test

--- Diplom_project_2024/Program.cs
+++ Diplom_project_2024/Program.cs
@@ -10,8 +10,17 @@ using Diplom_project_2024.AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
+DefaultAzureCredentialOptions options = new DefaultAzureCredentialOptions()
+{
+    Diagnostics =
+    {
+        LoggedHeaderNames = { "x-ms-request-id" },
+        LoggedQueryParameters = { "api-version" },
+        IsLoggingContentEnabled = true
+    }
+};
 var keyVaultEndpoint = new Uri("https://diplomproject2024vault.vault.azure.net/");
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential(options));
 
 
 
