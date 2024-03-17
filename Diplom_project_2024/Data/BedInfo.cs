a171a8bdfa810e5bdc7f053b1585c78066ba799d
The Waggle <artuoh76@gmail.com>
2024-03-17 20:07:50 +0100

    add bed info

--- Diplom_project_2024/Data/BedInfo.cs
+++ Diplom_project_2024/Data/BedInfo.cs
@@ -3,10 +3,11 @@
     public class BedInfo
     {
         public int Id { get; set; }
-        public int HouseId { get; set; }
-        public House House { get; set; } = new House();
+        //public int HouseId { get; set; }
+        //public House House { get; set; } = new House();
         public int BedTypeId { get; set; }
         public BedType BedType { get; set; } = new BedType();
         public int Quantity { get; set; }
+        public List<House>? Houses { get; set; }
     }
 }
