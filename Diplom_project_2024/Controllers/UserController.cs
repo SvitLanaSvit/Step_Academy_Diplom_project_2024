280525d21c729b56f943e753034b13383556fca1
The Waggle <artuoh76@gmail.com>
2024-03-28 13:46:06 +0100

    add full profile info

--- Diplom_project_2024/Controllers/UserController.cs
+++ Diplom_project_2024/Controllers/UserController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Diplom_project_2024.CustomErrors;
 using Microsoft.EntityFrameworkCore;
+using Diplom_project_2024.Models;
 
 namespace Diplom_project_2024.Controllers
 {
@@ -86,6 +87,131 @@ namespace Diplom_project_2024.Controllers
             if (payment == null) return NotFound(new Error("User doesn't have payment data"));
             var paymentDTO = mapper.Map<PaymentDataDTO>(payment);
             return Ok(paymentDTO);
+        }
+        [Authorize]
+        [HttpPost("SetFirstName")]
+        public async Task<IActionResult> SetFirstname(SetFirstNameModel firstname)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserFunctions.GetUser(userManager, User);
+                if (!string.IsNullOrEmpty(firstname.firstName))
+                {
+                    user.FirstName = firstname.firstName;
+                    await userManager.UpdateAsync(user);
+                    return Ok();
+                }
+                return BadRequest(new Error("Wrong firstname"));
+            }
+            return BadRequest(new Error("Required fields were not specified"));
+        }
+        [Authorize]
+        [HttpPost("SetSurname")]
+        public async Task<IActionResult> SetSurname(SetSurnameModel surname)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserFunctions.GetUser(userManager, User);
+                if (!string.IsNullOrEmpty(surname.surname))
+                {
+                    user.Surname = surname.surname;
+                    await userManager.UpdateAsync(user);
+                    return Ok();
+                }
+                return BadRequest(new Error("Wrong surname"));
+            }
+            return BadRequest(new Error("Required fields were not specified"));
+        }
+        [Authorize]
+        [HttpPost("SetGender")]
+        public async Task<IActionResult> SetGender(SetGenderModel gender)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserFunctions.GetUser(userManager, User);
+                if (!string.IsNullOrEmpty(gender.gender))
+                {
+                    user.Gender = gender.gender;
+                    await userManager.UpdateAsync(user);
+                    return Ok();
+                }
+                return BadRequest(new Error("Wrong gender"));
+            }
+            return BadRequest(new Error("Required fields were not specified"));
+        }
+        [Authorize]
+        [HttpPost("SetDateOfBirth")]
+        public async Task<IActionResult> SetDateOfBirth(SetDateOfBirthModel dateOfBirthModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (dateOfBirthModel.dateOfBirth == null) return BadRequest(new Error("Wrong date of birth"));
+                var user = await UserFunctions.GetUser(userManager, User);
+                user.DateOfBirth = dateOfBirthModel.dateOfBirth;
+                await userManager.UpdateAsync(user);
+                return Ok();
+            }
+            return BadRequest(new Error("Required fields were not specified"));
+        }
+        [Authorize]
+        [HttpPost("SetPhoneNumber")]
+        public async Task<IActionResult> SetPhoneNumber(SetPhoneNumber phoneNumber)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserFunctions.GetUser(userManager, User);
+                if (!string.IsNullOrEmpty(phoneNumber.phoneNumber))
+                {
+                    user.PhoneNumber = phoneNumber.phoneNumber;
+                    await userManager.UpdateAsync(user);
+                    return Ok();
+                }
+                return BadRequest(new Error("Wrong phone number"));
+            }
+            return BadRequest(new Error("Required fields were not specified"));
+        }
+        [Authorize]
+        [HttpPost("SetContactEmail")]
+        public async Task<IActionResult> SetContactEmail(SetContactEmail contactEmail)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserFunctions.GetUser(userManager, User);
+                if (!string.IsNullOrEmpty(contactEmail.contactEmail))
+                {
+                    user.ContactEmail = contactEmail.contactEmail;
+                    await userManager.UpdateAsync(user);
+                    return Ok();
+                }
+                return BadRequest(new Error("Wrong contact email"));
+            }
+            return BadRequest(new Error("Required fields were not specified"));
+        }
+        [Authorize]
+        [HttpGet("GetBasicInfo")]
+        public async Task<IActionResult> GetBasicInfo()
+        {
+            var user = await UserFunctions.GetUser(userManager, User);
+            UserBasicInfoDTO userBasicInfoDTO = new UserBasicInfoDTO()
+            {
+                dateOfBirth = user.DateOfBirth,
+                gender = user.Gender,
+                surname = user.Surname,
+                firstname = user.FirstName,
+            };
+            return Ok(userBasicInfoDTO);
+        }
+        [Authorize]
+        [HttpGet("GetContactInfo")]
+        public async Task<IActionResult> GetContactInfo()
+        {
+            var user = await UserFunctions.GetUser(userManager, User);
+            UserContactInfoDTO userContactInfoDTO = new UserContactInfoDTO()
+            {
+                phoneNumber = user.PhoneNumber,
+                contactEmail = user.ContactEmail,
+            };
+            return Ok(userContactInfoDTO);
         }
     }
 }
---
+++ Diplom_project_2024/Models/OneParameterModels.cs
@@ -0,0 +1,27 @@
+﻿namespace Diplom_project_2024.Models
+{
+    public class SetFirstNameModel
+    {
+        public string firstName { get; set; } = default!;
+    }
+    public class SetSurnameModel
+    {
+        public string surname { get; set; } = default!;
+    }
+    public class SetGenderModel
+    {
+        public string gender { get; set; } = default!;
+    }
+    public class SetDateOfBirthModel
+    {
+        public DateOnly? dateOfBirth { get; set; }
+    }
+    public class SetPhoneNumber
+    {
+        public string phoneNumber { get; set; } = default!;
+    }
+    public class SetContactEmail
+    {
+        public string contactEmail { get; set; } = default!;
+    }
+}
